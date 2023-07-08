18672a249f24c36eaee5dc8927391fca041bf9c9
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2023-07-08 23:14:54 +0700

    change

--- Binus Xplore/HomeView.swift
+++ Binus Xplore/HomeView.swift
@@ -6,3 +6,145 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreData
+
+
+struct HomeView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    struct Constants {
+        static let Primary: Color = Color(red: 0.02, green: 0.09, blue: 0.42)
+    }
+    
+    var body: some View {
+        VStack{
+            HStack{
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.mainBlueIcon)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "briefcase.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("Office")
+                        .foregroundStyle(.primary)
+                        .font(.title3)
+                        .padding()
+                }
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.mainBlueIcon)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "books.vertical.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("Class")
+                        .foregroundStyle(.primary)
+                        .font(.title3)
+                        .padding()
+                }
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.mainBlueIcon)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "pc")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("Labs")
+                        .foregroundStyle(.primary)
+                        .font(.title3)
+                        .padding()
+                }
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.mainBlueIcon)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "person.2.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("Public Facility")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.primary)
+                        .font(.subheadline)
+                        .padding()
+                }
+            }
+            HStack{
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.colorIcon2)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "building.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("A Building")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.primary)
+                        .font(.subheadline)
+                        .padding()
+                }
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.colorIcon2)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "building.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("B Building")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.primary)
+                        .font(.subheadline)
+                        .padding()
+                }
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.colorIcon2)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "building.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("C Building")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.primary)
+                        .font(.subheadline)
+                        .padding()
+                }
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.colorIcon3)
+                            .frame(width: 64, height: 64)
+                        Image(systemName: "cup.and.saucer.fill")
+                            .frame(width: 46.0, height: 46.0)
+                            .foregroundStyle(.white)
+                    }
+                    Text("F&B")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.primary)
+                        .font(.subheadline)
+                        .padding()
+                }
+            }
+        }
+    }
+}
+        
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
