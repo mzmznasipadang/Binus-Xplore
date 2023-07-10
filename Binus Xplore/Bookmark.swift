7cd9b4a856373575af35c03db0cc9f20b59b77de
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2023-07-10 22:29:55 +0700

    Bookmark Finally done (?)

--- Binus Xplore/Bookmark.swift
+++ Binus Xplore/Bookmark.swift
@@ -1,161 +1,51 @@
-////
-////  Bookmark.swift
-////  Binus Xplore
-////
-////  Created by Victor Chandra on 07/07/23.
-////
 //
-//import Foundation
-//import SwiftUI
+//  bookmark tes.swift
+//  Binus Xplore
 //
-////struct Constants {
-////    static let Primary: Color = Color(red: 0, green: 0.29, blue: 0.68)
-////}
+//  Created by Jessica Lynn on 10/07/23.
 //
-//struct Bookmark: View {
-//    @State private var animate = false
+
+import Foundation
 //
-//    var body: some View {
-//        ZStack {
-//            createGrid(offset: animate ? -UIScreen.main.bounds.width : 0)
-//            createGrid(offset: animate ? 0 : UIScreen.main.bounds.width)
-//        }
-//        .onAppear {
-//            withAnimation(Animation.linear(duration: 5).repeatForever(autoreverses: false)) {
-//                animate = true
-//            }
-//        }
-//    }
+//  ContentView.swift
+//  bookmark tes
 //
-//    func createGrid(offset: CGFloat) -> some View {
-//        HStack{
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
+//  Created by Jessica Lynn on 07/07/23.
 //
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.red)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.red)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.red)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.red)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.red)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.blue)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.red)
-//            VStack {
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//                Image(systemName: "iphone")
-//                Image(systemName: "iphone.gen2")
-//            }.foregroundStyle(.green)
-//
-////            VStack {
-////                Image(systemName: "iphone.gen2")
-////                Image(systemName: "iphone")
-////                Image(systemName: "iphone.gen2")
-////                Image(systemName: "iphone")
-////                Image(systemName: "iphone.gen2")
-////            }.foregroundStyle(.yellow)
-//        }
-//
-//        .offset(x: offset)
-//
-//    }
-//}
-//
-//
-//
-//    struct Bookmark_Previews: PreviewProvider {
-//        static var previews: some View {
-//            Bookmark()
-//        }
-//    }
+
+import SwiftUI
+
+struct Bookmark2: View {
+    var body: some View {
+        NavigationView {
+                VStack{
+                    VStack{ //Using VStack instead of ZStack
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 354, height: 253)
+                            .background(
+                                Image("404")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 354, height: 253)
+                                    .clipped()
+                            )
+                        Text("Any favorite spot in mind?")
+                            .font(Font.custom("SF Pro", size: 18))
+                            .foregroundColor(Color(red: 0.32, green: 0.32, blue: 0.32))
+                    }.navigationTitle("Bookmarked Places")
+                    Spacer()
+                    navbar(selectedTab: .bookmark)
+                }
+            }
+        
+        .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct Bookmark2_Previews: PreviewProvider {
+    static var previews: some View {
+        Bookmark2()
+    }
+}
