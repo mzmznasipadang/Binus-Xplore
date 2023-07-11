633d11a75db79d4797d1740bd7995d8dccdfb9d7
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2023-07-11 18:02:15 +0700

    kata ibu negara suruh ganti

--- Binus Xplore/Bookmark.swift
+++ Binus Xplore/Bookmark.swift
@@ -1,5 +1,5 @@
 //
-//  Bookmark.swift
+//  BookmarkSwift
 //  Binus Xplore
 //
 //  Created by Jessica Lynn on 10/07/23.
@@ -10,40 +10,86 @@ import SwiftUI
 
 struct Bookmark: View {
     
-    @State var selectedTab: Tab = .bookmark
+//    enum Tab {
+//        case bookmark, home, profile
+//    }
+//
+//    var selectedTab: Tab
     
     var body: some View {
-        NavigationView {
-                VStack{
-                    VStack{ //Using VStack instead of ZStack
-                        Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 354, height: 400)
-                            .background(
-                                Image("404")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 354, height: 253)
-                                    .clipped()
-                            ).offset(y:60)
-                        Text("Any favorite spot in mind?")
-                            .foregroundColor(Color(red: 0.32, green: 0.32, blue: 0.32))
-                            .font(.system(size: 20, weight: .medium, design: .default))
-                            
-                    }.offset(y: 10)
-                    .navigationTitle("Bookmarked Places")
-                    
-                    Spacer()
-                    TabBar(selectedTab: $selectedTab)
-                }
-        }
         
-        .background(Color(red: 0.97, green: 0.97, blue: 0.97))
-        .edgesIgnoringSafeArea(.bottom)
+        NavigationView{
+//            ZStack {
+                // Your other views go here
+            VStack {
+                VStack{
+                    Spacer()
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 354, height: 253)
+                        .background(
+                            Image("404")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 354, height: 253)
+                                .clipped()
+                        )
+                    Text("Any favorite spot in mind?")
+                        .font(Font.custom("SF Pro", size: 18))
+                        .foregroundColor(Color(red: 0.32, green: 0.32, blue: 0.32))
+                    Spacer()
+                }
+                .safeAreaInset(edge: .bottom){
+                    //batas antara content sama navbar
+                    ZStack{
+                        Rectangle()
+                            .fill(.white)
+                            .frame(height: 94)
+                        HStack{
+                            //                            NavigationLink(destination: Bookmark2()){
+                            Image(systemName: "bookmark.fill")
+                                .font(Font.custom("SF Pro", size: 40))
+                                .foregroundColor(Color(red: 0, green: 0.29, blue: 0.68))
+                            //                            }
+                            Spacer()
+                            NavigationLink(destination: HomeView()){
+                                ZStack{
+                                    Circle()
+                                        .fill(Color(red: 0.53, green: 0.73, blue: 1))
+                                        .frame(width: 76, height: 76)
+                                        .offset(y:-25)
+                                        .overlay(
+                                            Circle().stroke(Color.white, lineWidth: 7).offset(y:-25)
+                                        )
+                                    Image(systemName: "house.fill")
+                                        .font(Font.custom("SF Pro", size: 47))
+                                        .foregroundColor(.white)
+                                        .offset(y:-28)
+                                }
+                            }
+                            Spacer()
+                            
+                            NavigationLink(destination: Onboarding2()){
+                                Image(systemName: "person")
+                                    .font(Font.custom("SF Pro", size: 40))
+                                    .foregroundColor(Color(red: 0.53, green: 0.73, blue: 1))
+                            }
+                        }.padding(.horizontal, 78.0)
+                    }
+                }
+                .edgesIgnoringSafeArea(.bottom)
+                .frame(maxHeight: .infinity)
+                .navigationTitle("Bookmarked Places")
+            }
+            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+//            }.navigationTitle("Bookmarked Places")
+        }
+        .navigationBarBackButtonHidden(true)
+
     }
 }
 
-struct Bookmark_Previews: PreviewProvider {
+struct BookMark_Previews: PreviewProvider {
     static var previews: some View {
         Bookmark()
     }
--- Binus Xplore/HomeView.swift
+++ Binus Xplore/HomeView.swift
@@ -16,141 +16,195 @@ struct HomeView: View {
         static let Primary: Color = Color(red: 0.02, green: 0.09, blue: 0.42)
     }
     
-    @State var selectedTab: Tab = .home
-    
     var body: some View {
-        NavigationView {
-            VStack{
-                HStack{
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.blue)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "briefcase.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
+        NavigationView{
+            ZStack{
+                VStack{
+                    HStack(alignment: .top, spacing: 25){
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.blue)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "briefcase.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("Office")
+                                .foregroundStyle(.primary)
+                            //                            .font(.title3)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
                         }
-                        Text("Office")
-                            .foregroundStyle(.primary)
-                            .font(.title3)
-                            .padding()
+                        
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.blue)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "books.vertical.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("Class")
+                                .foregroundStyle(.primary)
+                            //                            .font(.title3)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
+                        }
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.blue)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "pc")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("Labs")
+                                .foregroundStyle(.primary)
+                            //                            .font(.title3)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
+                        }
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.blue)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "person.2.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("Public\nFacility")
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.primary)
+                            //                            .font(.subheadline)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
+                        }
                     }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.blue)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "books.vertical.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
+                    HStack(alignment: .top, spacing: 25){
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.red)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "building.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("A Building")
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.primary)
+                            //                            .font(.subheadline)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
                         }
-                        Text("Class")
-                            .foregroundStyle(.primary)
-                            .font(.title3)
-                            .padding()
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.red)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "building.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("B Building")
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.primary)
+                            //                            .font(.subheadline)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
+                        }
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.red)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "building.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("C Building")
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.primary)
+                            //                            .font(.subheadline)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
+                        }
+                        VStack{
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.green)
+                                    .frame(width: 64, height: 64)
+                                Image(systemName: "cup.and.saucer.fill")
+                                    .frame(width: 46.0, height: 46.0)
+                                    .foregroundStyle(.white)
+                            }
+                            Text("F&B")
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.primary)
+                            //                            .font(.subheadline)
+                                .font(Font.custom("SF Pro", size: 12))
+                                .padding()
+                        }
                     }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.blue)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "pc")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
-                        }
-                        Text("Labs")
-                            .foregroundStyle(.primary)
-                            .font(.title3)
-                            .padding()
-                    }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.blue)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "person.2.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
-                        }
-                        Text("Public Facility")
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.primary)
-                            .font(.subheadline)
-                            .padding()
+                    
+                }
+                
+                //batas content dan navbar
+                
+                VStack{
+                    Spacer()
+                    ZStack{
+                        Rectangle()
+                            .fill(.white)
+                            .frame(height: 94)
+                        HStack{
+                            NavigationLink(destination: Bookmark()){
+                                Image(systemName: "bookmark")
+                                    .font(Font.custom("SF Pro", size: 40))
+                                    .foregroundColor(Color(red: 0.53, green: 0.73, blue: 1))
+                            }
+                            Spacer()
+                            //                        NavigationLink(destination: HomeView()){
+                            ZStack{
+                                Circle()
+                                    .fill(Color(red: 0, green: 0.29, blue: 0.68))
+                                    .frame(width: 76, height: 76)
+                                    .offset(y:-25)
+                                    .overlay(
+                                        Circle().stroke(Color.white, lineWidth: 7).offset(y:-25)
+                                    )
+                                Image(systemName: "house.fill")
+                                    .font(Font.custom("SF Pro", size: 47))
+                                    .foregroundColor(.white)
+                                    .offset(y:-28)
+                            }
+                            //                        }
+                            Spacer()
+                            
+                            NavigationLink(destination: Onboarding2()){
+                                Image(systemName: "person")
+                                    .font(Font.custom("SF Pro", size: 40))
+                                    .foregroundColor(Color(red: 0.53, green: 0.73, blue: 1))
+                            }
+                        }.padding(.horizontal, 78.0)
                     }
                 }
-                HStack{
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.red)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "building.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
-                        }
-                        Text("A Building")
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.primary)
-                            .font(.subheadline)
-                            .padding()
-                    }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.red)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "building.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
-                        }
-                        Text("B Building")
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.primary)
-                            .font(.subheadline)
-                            .padding()
-                    }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.red)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "building.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
-                        }
-                        Text("C Building")
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.primary)
-                            .font(.subheadline)
-                            .padding()
-                    }
-                    VStack{
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.green)
-                                .frame(width: 64, height: 64)
-                            Image(systemName: "cup.and.saucer.fill")
-                                .frame(width: 46.0, height: 46.0)
-                                .foregroundStyle(.white)
-                        }
-                        Text("F&B")
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.primary)
-                            .font(.subheadline)
-                            .padding()
-                    }
-                }
-                Spacer()
-                TabBar(selectedTab: $selectedTab)
-            }.navigationTitle("Explore BINUS")
-                .navigationBarBackButtonHidden(true)
-                .navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.bottom)
+                .frame(maxHeight: .infinity)
+            }
+            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+            .navigationTitle("Explore BINUS")
+
+            
         }
+        .navigationBarBackButtonHidden(true)
     }
 }
-
+        
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
