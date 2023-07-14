a59ab1d0cd3424d90adfb89612ae9a0588b8bafd
Jessica L <jeslynn30@gmail.com>
2023-07-14 15:56:18 +0700

    add login regis file animate splash

--- Binus Xplore/Onboarding/Splash.swift
+++ Binus Xplore/Onboarding/Splash.swift
@@ -11,34 +11,43 @@ import SwiftUI
 
 struct Splash: View {
     
-    @State var isActive: Bool = false
+//    @State var isActive: Bool = false
+    @State private var isActive = false
+    @State private var size = 0.1
+    @State private var opacity = 0.5
     
-    var body: some View {
-        HStack{
-            
-            if self.isActive{
-                Onboarding()
-            } else {
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 300, height: 300)
-                    .background(
-                        Image("Logo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 300, height: 300)
-                            .clipped()
-                    )
-            }
+    
+    var body: some View{
+        if isActive{
+            Onboarding()
         }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5){
-                withAnimation(.linear){
-                    self.isActive = true
-                        
+        else{
+            ZStack{
+                Color.white
+                    .ignoresSafeArea()
+                VStack{
+                    VStack{
+                        Image("Logo")
+                            .frame(width:30.0, height: 30.0)
+                    }
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .onAppear{
+                        withAnimation(.easeIn(duration:1.2)){
+                            self.size=0.6
+                            self.opacity=1.0
+                        }
+                    }
+                }
+                .onAppear{
+                    DispatchQueue.main.asyncAfter(deadline: .now()+3.5){
+                        self.isActive=true
+                    }
                 }
             }
+
         }
+
     }
 }
 
